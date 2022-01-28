66fadb2839bd13656e9b2f0498bc38a7e32c0945
bimashazaman <bimashazaman1234@gmail.com>
2022-01-28 06:43:30 +0600

    yayyy i'm pro now

---
+++ HashTable.java
@@ -0,0 +1,36 @@
+// what is a hash table??
+
+// HashTable is a data structure that stores key-value pairs.
+// The key is used to look up the value.
+// The value is the data that is stored.
+// The key must be unique.
+// The key can be any type of object.
+// The value can be any type of object.
+// The value can be null.
+
+//Ht ARE OFTEN USED TO TRACK ITEM FREQUENCIES. FOR INTENCE COUNTING THE NUMBER OF A WORD APPEARANCE IN A TEXT.
+
+//what is a hash function??
+//A hash function is a function that maps a key IN TO A FIXED RANGE
+
+//properties of hash table
+// IF H(x) =  H(y) THEN x = y 
+// H(x) != H(y)  // if H(x) != H(y) then x != y
+
+// separate chaining and open addressing 
+
+
+
+// complexity of hash table
+
+//insertion: O(1)  worst case O(n)
+//deletion: O(1)   worst case O(n)
+//finding: O(1)         worst case O(n)
+//contains: O(1)        worst case O(n)
+//size: O(1)        worst case O(n)
+//isEmpty: O(1)     worst case O(n)
+//clear: O(n)       worst case O(n)
+
+
+
+//
